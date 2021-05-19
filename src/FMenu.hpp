7cdc8b0bb3fd6a47b636e445a91070524c7adf67
Luke Eltiste <luke343279@gmail.com>
2021-05-19 08:42:57 -0700

    start of outline

--- src/FMenu.hpp
+++ src/FMenu.hpp
@@ -2,6 +2,15 @@
 #define MENU_H
 #pragma once
 
+class FMenu
+{
+  FMenu();    // Constructor
 
+  void runMenu();    // Starts the Menu
+
+public:
+  // Variables
+  char quit_key = 'q';
+};
 
 #endif
